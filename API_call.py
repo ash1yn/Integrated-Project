1ee2df5df380ad0b456e12a1edc114070b5a9764
Mikey-571918 <s10240150@connect.np.edu.sg>
2022-07-31 15:40:56 +0800

    API key

--- API_call.py
+++ API_call.py
@@ -1,11 +1,10 @@
 import requests 
 
 api_key =  "GJ9XBY2S6TIDBORV"
-url_1 = "https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=IBM&apikey={api_key}"
-url_2 = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={api_key}"
+url = "https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol=USD&to_symbol=SGD&apikey={api_key}"
 
-response_1 = requests.get(url_1)
-response_2 = requests.get(url_2)
-print(response_1) # returns <Response [200]> which means it is a successful API call
-print(response_2) # returns <Response [200]> which means it is a successful API call
+response = requests.get(url)
+print(response) # returns <Response [200]> which means it is a successful API call
+
+print(response.json())
 
