4254c0fe247f54419016bd8f1c8d854ccc9ea7a5
Mikey-571918 <s10240150@connect.np.edu.sg>
2022-07-31 23:20:23 +0800

    read files

--- read_file.py
+++ read_file.py
@@ -1,1 +1,11 @@
-print("new")
+from pathlib import Path
+import re,csv 
+
+file_path_1 = Path.cwd()/"csv_reports"/"pfb_overheads.csv"
+print(file_path_1.exists())
+
+with file_path_1.open(mode = "r", encoding = "UTF-8") as file:
+    for line in file.readlines():
+        print(line)
+
+
