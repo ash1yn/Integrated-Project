dd13621595c3f356b3e7f391253560eaa3f167a8
Mikey-571918 <s10240150@connect.np.edu.sg>
2022-07-31 23:32:43 +0800

    read files

--- read_file.py
+++ read_file.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import re,csv 
 
-file_path_1 = Path.cwd()/"csv_reports"/"Overheads.csv"
+file_path_1 = Path.cwd()/"csv_reports"/"Profit & Loss.csv"
 print(file_path_1.exists())
 
 with file_path_1.open(mode = "r", encoding = "UTF-8") as file:
@@ -13,6 +13,14 @@ file_path_2 = Path.cwd()/"csv_reports"/"Cash on Hand.csv"
 print(file_path_2.exists())
 
 with file_path_2.open(mode = "r", encoding = "UTF-8") as file:
+    reader = csv.reader(file)
+    for line in reader:
+        print(line)
+
+file_path_3 = Path.cwd()/"csv_reports"/"Overheads.csv"
+print(file_path_3.exists())
+
+with file_path_3.open(mode = "r", encoding = "UTF-8") as file:
     reader = csv.reader(file)
     for line in reader:
         print(line)
