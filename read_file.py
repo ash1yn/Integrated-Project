31165f37591b6213839f46557fd097e82cadf4b4
Mikey-571918 <s10240150@connect.np.edu.sg>
2022-07-31 23:36:46 +0800

    read

--- read_file.py
+++ read_file.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import re,csv 
 
-file_path_1 = Path.cwd()/"csv_reports"/"Profit & Loss.csv"
+file_path_1 = Path.cwd()/"csv_reports"/"Profit and Loss.csv"
 print(file_path_1.exists())
 
 with file_path_1.open(mode = "r", encoding = "UTF-8") as file:
