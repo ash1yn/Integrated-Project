2452a3609ac9e05aae676b975371fb6ee22c5ed0
Meng Yik <107129298+Mikey-571918@users.noreply.github.com>
2022-07-31 14:59:35 +0800

    Update modA.py

--- modA.py
+++ modA.py
@@ -1,3 +1,11 @@
-def add (a, b):
-    return a + b
-    hello
+import requests 
+
+api_key =  "GJ9XBY2S6TIDBORV"
+url_1 = "https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=IBM&apikey={api_key}"
+url_2 = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={api_key}"
+
+response_1 = requests.get(url_1)
+response_2 = requests.get(url_2)
+print(response_1) # returns <Response [200]> which means it is a successful API call
+print(response_2) # returns <Response [200]> which means it is a successful API call
+
