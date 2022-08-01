082f9d36fb7976064bd80396164133b0d055b75e
Mikey-571918 <s10240150@connect.np.edu.sg>
2022-08-01 15:48:27 +0800

    for loop

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import re, csv
+import string
 from api import convertUSDtoSGD
 
 cash_on_hand = Path.cwd()/"csv_reports"/"Cash on Hand.csv"
@@ -15,11 +16,11 @@ with cash_on_hand.open(mode = "r", encoding = "UTF-8", newline = "") as file:
         day.append(float(line[0]))
         cash.append(float(line[1]))
 
-        for value in range (day, cash):
-            if cash[value] < cash[value-1]:
-                print(f"*Day: {day(value)}, Value Difference: {cash[value-1]-cash[value]}*")
-            else:
-                continue
+    for value in range (day, cash):
+        if cash[value] < cash[value-1]:
+            print(f"*Day: {day(value)}, Value Difference: {cash[value-1]-cash[value]}*")
+        else:
+            print("Cash on hand for each period is higher than the previous period")
 
 
 
