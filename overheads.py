3ba15adf575a20e4f4f753b63b017f1107faee5d
Mikey-571918 <s10240150@connect.np.edu.sg>
2022-08-01 22:13:00 +0800

    overheads

--- overheads.py
+++ overheads.py
@@ -1,11 +1,24 @@
 from pathlib import Path
 import re, csv
+from api import convertUSDtoSGD
 
 overheads = Path.cwd()/"csv_reports"/"Overheads.csv"
 print(overheads.exists())
+
+overheads_list = []
+highest = 0
 
 with overheads.open(mode = "r", encoding = "UTF-8") as file:
     reader = csv.reader(file)
     next(reader)
     for line in reader:
-        print(line)
+        # print(line)
+        overheads_list.append([line[0],float(line[1])])
+
+    last_index = len(overheads_list)
+
+    for i in range(last_index):
+        if overheads_list[i][1] > highest:
+            highest = overheads_list[i][1]
+            print(f"{overheads_list[i][0]} : SGD {convertUSDtoSGD(highest)}")
+    
