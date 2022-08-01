cd17281008f3d3098d4e050816cfb9c6ae7e29fe
Mikey-571918 <s10240150@connect.np.edu.sg>
2022-08-01 12:32:57 +0800

    api

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 import requests 
 
 api_key =  "GJ9XBY2S6TIDBORV"
-url = "https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol=USD&to_symbol=SGD&apikey={api_key}"
+url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={api_key}"
 
 response = requests.get(url)
 print(response) # returns <Response [200]> which means it is a successful API call
