216d8f7c71699957beb7ff5d1c4b8a5b925559b9
Mikey-571918 <s10240150@connect.np.edu.sg>
2022-08-02 17:48:01 +0800

    main

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ summary_report = Path.cwd()/"summary_report.txt"
 print(summary_report.exists())
 
 with summary_report.open(mode = "w", encoding = "UTF-8", newline = "") as file:
-    # file.write(overheads_summary_report)
+    file.write(overheads_summary_report())
     file.write(cash_on_hand_summary_report())
-    file.write(profits_and_loss_summary_report)
+    file.write(profits_and_loss_summary_report())
 
