ede9b04eb67fcbf9ab4bb1d7eb5b692a96071353
Mikey-571918 <s10240150@connect.np.edu.sg>
2022-08-02 13:00:58 +0800

    api

--- api.py
+++ api.py
@@ -3,12 +3,15 @@ import requests
 api_key =  "GJ9XBY2S6TIDBORV"
 url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={api_key}"
 
-response = requests.get(url)
-print(response) # returns <Response [200]> which means it is a successful API call
-data = response.json()
+response = requests.get(url) 
+print(response) 
+# made a successful API call (ready to extract the required forex data)
+data = response.json() 
+# returns nested dictionary in a json object
 print(data)
 
 USDtoSGD_Exchange_Rate = data['Realtime Currency Exchange Rate']['5. Exchange Rate']
+# Used to find the exchange rate from USD to SGD found in the nested dictionary
 
 def convertUSDtoSGD(USD):
     """
