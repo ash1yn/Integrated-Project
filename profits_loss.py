cf7530a9218e9b3e8a7c861b01e3e34a0924248d
Mikey-571918 <s10240150@connect.np.edu.sg>
2022-08-02 13:36:20 +0800

    profits and loss

--- profits_loss.py
+++ profits_loss.py
@@ -4,16 +4,21 @@ import re, csv
 from api import convertUSDtoSGD
 
 profits_loss = Path.cwd()/"csv_reports"/"Profit and Loss.csv"
+# Create a file path to the "Profit and Loss.csv" in the "csv_reports" folder
 print(profits_loss.exists())
 
 day_profit_list = []
+# Create an empty list to append
 
 with profits_loss.open(mode = "r", encoding = "UTF-8") as file:
+# opens the "Profits and Loss.csv" file in read mode
     reader = csv.reader(file)
+    # Reads the file
     next(reader)
+    # Skips the header
     for line in reader:
-        # print(line)
         day_profit_list.append([int(line[0]),int(line[4])])
+        # Appends the day and net profit values, found in the "Profit and Loss.csv" into the empty list (in a nested list format)
 
     last_index = len(day_profit_list)-1
 
@@ -21,9 +26,12 @@ with profits_loss.open(mode = "r", encoding = "UTF-8") as file:
 
     for i in range(last_index):
         if day_profit_list[i][1] > day_profit_list[i+1][1]:
+        # If the net profit value is not consecutively higher each day, 
             print(f"* DAY: {day_profit_list[i+1][0]}, AMOUNT: SGD {convertUSDtoSGD(day_profit_list[i][1]-day_profit_list[i+1][1])} *")
+            # The day where net profit is lower than the previous day and the value difference will be highlighted
             all_higher = FALSE
 
     if all_higher == TRUE:
+    # If net profit value is consecutively higher each day,
         print("Net Profit for each period is higher than the previous period")
     
