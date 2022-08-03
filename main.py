72fd7f56cf4eecc485bb72fead779fab384b02be
ash1yn <s10240055@connect.np.edu.sg>
2022-08-03 11:56:21 +0800

    -

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ with summary_report.open(mode = "w", encoding = "UTF-8", newline = "") as file:
     # Writes the highest overheads category and value into the "summary_report.txt" file
     
     if len(cash_on_hand) == 0:
-        file.write(f"[CASH SURPLUS] Cash on hand for each period is higher than the previous period\n")
-    for l in range (len(cash_on_hand)):
+        file.write(f"[CASH SURPLUS] Cash on hand for each period is higher than the previous period\n") #
+    for l in range (len(cash_on_hand)): 
         file.write(f"[CASH DEFICIT] DAY: {cash_on_hand[l][0]}, AMOUNT: SGD {cash_on_hand[l][1]}\n")
     
     if len(profits_and_loss) == 0:
