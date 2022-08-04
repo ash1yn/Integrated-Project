e403d8141afc3aa3d979701835351ae5f0464a55
alinaliau <109326422+alinaliau@users.noreply.github.com>
2022-08-04 16:18:04 +0800

    api

--- api.py
+++ api.py
@@ -29,6 +29,7 @@ def exchange_rate():
     """
     - returns the exchange rate extracted from the API
     """
+    
     try:
         
         USDtoSGD_Exchange_Rate = float(data['Realtime Currency Exchange Rate']['5. Exchange Rate'])
