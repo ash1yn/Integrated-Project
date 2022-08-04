73452be35743154326421c74281b3a0119b803cd
alinaliau <109326422+alinaliau@users.noreply.github.com>
2022-08-04 16:07:08 +0800

    overheads

--- overheads.py
+++ overheads.py
@@ -5,7 +5,9 @@ from api import convertUSDtoSGD
 highest = 0 
 
 def overheads_summary_report():
-
+    """
+    - Return the overhead category and its value, from the data given in the "Overheads.csv" file
+    """
     try:
         global highest
         
