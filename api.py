69f41e3a13234b73709fee772c77dc434f0787dc
Mikey-571918 <s10240150@connect.np.edu.sg>
2022-08-04 15:52:02 +0800

    api

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 import requests 
 from pathlib import Path
 
-api_key =  "G9O4IFGP94PHD0IR"
+api_key =  "K6YIIIAHV95WGM9K"
 url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={api_key}"
 
 response = requests.get(url) 
@@ -30,7 +30,7 @@ def exchange_rate():
         """
         USDtoSGD_Exchange_Rate = float(data['Realtime Currency Exchange Rate']['5. Exchange Rate'])
         # Used to find the exchange rate from USD to SGD found in the nested dictionary
-        
+
         return USDtoSGD_Exchange_Rate
 
     except Exception as e:
